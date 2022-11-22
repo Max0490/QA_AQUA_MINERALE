954fe4a9b107ade9712b6117b65606527ea0dbd8
USER <seagate90@mail.ru>
2022-11-22 18:27:52 +0400

    add utf

--- src/test/java/SberTest.java
+++ src/test/java/SberTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -9,6 +10,13 @@ public class SberTest {
     @Test
     public void openPage() {
 
+        open("http://www.sberbank.ru/ru/person");
+        $(".kitt-header__sbol").shouldHave(text("СберБанк Онлайн"));
+        //$(byLinkText("Офисы")).click();
+    }
+
+    @Test
+    public void searchOffice() {
         open("http://www.sberbank.ru/ru/person");
         $(byLinkText("Офисы")).click();
     }
